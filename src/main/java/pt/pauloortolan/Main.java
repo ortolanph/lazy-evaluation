aadd0456ebb41ef9bc527689f5049e5c87140d84
Paulo Henrique Ortolan <paulo.ortolan@gmail.com>
2025-11-27 10:02:42 +0000

    main code

--- src/main/java/pt/pauloortolan/Main.java
+++ src/main/java/pt/pauloortolan/Main.java
@@ -1,9 +1,72 @@
 package pt.pauloortolan;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import pt.pauloortolan.loader.RevenueLoader;
+import pt.pauloortolan.service.ClassicMarketService;
+import pt.pauloortolan.service.MarketService;
+
 public class Main {
+
+    public static final String RIGHT_FORMATTED = "%30s%n";
+    public static final String NUMBER_FORMAT = "%,.2f";
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        RevenueLoader loader = new RevenueLoader("supermarket_revenue.json");
+
+        double result = 0.0;
+        String formattedResult = "";
+
+        MarketService service = new ClassicMarketService(loader.getMarketData());
+
+        System.out.println("ALL COUNTRIES");
+        service
+                .getAllCountries()
+                .forEach(System.out::println);
+        System.out.println("----------------------------------------");
+
+        System.out.println();
+        System.out.println("ALL COUNTRIES");
+        service
+                .getStoresByCountry(country -> country.getName().equals("Portugal"))
+                .forEach(System.out::println);
+        System.out.println("----------------------------------------");
+
+        System.out.println();
+        System.out.println("ALL LOCATIONS BY COUNTRY");
+        service
+                .getLocationsByCountry(country -> country.getName().equals("Portugal"))
+                .forEach(System.out::println);
+        System.out.println("----------------------------------------");
+
+        System.out.println();
+        System.out.println("ALL SECTIONS BY STORE");
+        service
+                .getSectionsByStore(store -> store.getId().equals("PT001"))
+                .forEach(System.out::println);
+        System.out.println("----------------------------------------");
+
+        System.out.println();
+        System.out.println("TOTAL REVENUE");
+        result = service.getTotalRevenue();
+        formattedResult = String.format(NUMBER_FORMAT, result);
+        System.out.printf(RIGHT_FORMATTED, formattedResult);
+        System.out.println("----------------------------------------");
+
+        System.out.println();
+        System.out.println("TOTAL REVENUE BY COUNTRY");
+        result = service.getTotalRevenueByCountry(country -> country.getName().equals("Portugal"));
+        formattedResult = String.format(NUMBER_FORMAT, result);
+        System.out.printf(RIGHT_FORMATTED, formattedResult);
+        System.out.println("----------------------------------------");
+
+        System.out.println();
+        System.out.println("TOTAL REVENUE BY COUNTRY AND STORE");
+        result = service
+                .getTotalRevenueByCountryAndStore(
+                        country -> country.getName().equals("Portugal"),
+                        store -> store.getId().equals("PT001")
+                );
+        formattedResult = String.format(NUMBER_FORMAT, result);
+        System.out.printf(RIGHT_FORMATTED, formattedResult);
+        System.out.println("----------------------------------------");
     }
 }
--- src/main/java/pt/pauloortolan/loader/RevenueLoader.java
+++ src/main/java/pt/pauloortolan/loader/RevenueLoader.java
@@ -1,12 +1,12 @@
 package pt.pauloortolan.loader;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Data;
+import lombok.Getter;
 import pt.pauloortolan.pojo.Market;
 
 import java.io.IOException;
 
-@Data
+@Getter
 public class RevenueLoader {
 
     private Market marketData;
@@ -16,8 +16,8 @@ public class RevenueLoader {
 
         try {
             marketData = mapper.readValue(
-                this.getClass().getClassLoader().getResourceAsStream(fileName),
-                Market.class
+                    this.getClass().getClassLoader().getResourceAsStream(fileName),
+                    Market.class
             );
         } catch (IOException exception) {
             System.out.printf(exception.getMessage());
